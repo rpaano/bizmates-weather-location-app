ecb2aa914e2d6fa60bf094c61f91b40c3b6c376f
Romel <50399801+rpaano@users.noreply.github.com>
2023-11-23 15:37:59 +0800

    Initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="flex justify-center">
+    Sample
+  </h1>
+</template>
---
+++ resources/js/router.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from "vue-router"
+import AppVue from "./App.vue";
+
+const routes = [
+  {
+    path : "/",
+    component: AppVue
+  },
+]
+
+const router = createRouter({
+  history : createWebHistory(),
+  routes
+})
+
+export default router;
