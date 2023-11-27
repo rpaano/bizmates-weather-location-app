db2a4860aed4d7b8dfd22d2fc6bb01ad5d6d4f4f
Romel <50399801+rpaano@users.noreply.github.com>
2023-11-27 10:31:03 +0800

    Update to component

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -5,15 +5,7 @@
         <div class="wrapper-info">
           <span class=" text-6xl font-bold leading-[1.2] font-montse text-white text-center">Discover Recommended<br />Cities</span>
           <h6 class="text-primary font-semibold font-montse text-center mb-2">platform for creatives around the world</h6>
-          <div class="flex justify-center mt-4 pt-2 mb-5 w-full">
-              <div class="input-group input-group-lg">
-                  <span class="input-group-text bi-search" id="basic-addon1">
-                  </span>
-                  <input name="city" autocomplete="off" type="search" class="form-control" id="city" placeholder="Tokyo, Yokohama, Kyoto, Osaka ..." aria-label="Search">
-
-                  <button @click="showVenuesToggle()" class="form-control">Search</button>
-              </div>
-          </div>
+          <Search @searchValue="showVenuesToggle" />
         </div>
         <transition nname="fadeHeight" :css="false" @before-enter="beforeEnter" @enter="enter">
           <div v-if="showVenues"  class="wrapper--ventue max-h-96 flex flex-wrap gap-x-3 gap-y-5 px-9 w-full">
@@ -43,10 +35,11 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
+import Search from './components/Search.vue';
 
 const showVenues = ref(false)
 
-const showVenuesToggle = () => {
+const showVenuesToggle = (inputValue) => {
   showVenues.value = !showVenues.value
 }
 
---
+++ resources/js/components/Search.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex justify-center mt-4 pt-2 mb-5 w-full">
+      <div class="input-group input-group-lg">
+          <span class="input-group-text bi-search" id="basic-addon1">
+          </span>
+          <input v-model="inputValue" name="city" autocomplete="off" type="search" class="form-control" id="city" placeholder="Tokyo, Yokohama, Kyoto, Osaka ..." aria-label="Search">
+
+          <button @click="emit('searchValue', inputValue)" class="form-control">Search</button>
+      </div>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const inputValue = ref()
+  const emit = defineEmits(['completeTodo'])
+</script>
