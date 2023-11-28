ce26e6af928e4524260cb2ff480067310b4345bc
Romel <50399801+rpaano@users.noreply.github.com>
2023-11-28 11:56:44 +0800

    Fix indent

--- resources/js/components/Search.vue
+++ resources/js/components/Search.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="flex justify-center mt-4 pt-2 mb-5 w-full">
-      <div class="input-group input-group-lg">
-          <span class="input-group-text bi-search" id="basic-addon1">
-          </span>
-          <input 
-            v-model="inputValue" 
-            name="city" 
-            autocomplete="off" 
-            type="search" 
-            class="form-control" 
-            id="city" 
-            placeholder="Tokyo, Yokohama, Kyoto, Osaka ..." 
-            aria-label="Search"
-          />
-          <button :disabled="searchLoading" @click="buttonSearch" class="form-control button-loading">
-            <span>Search</span>
-          </button>
-      </div>
+    <div class="input-group input-group-lg">
+      <span class="input-group-text bi-search" id="basic-addon1">
+      </span>
+      <input 
+        v-model="inputValue" 
+        name="city" 
+        autocomplete="off" 
+        type="search" 
+        class="form-control" 
+        id="city" 
+        placeholder="Tokyo, Yokohama, Kyoto, Osaka ..." 
+        aria-label="Search"
+      />
+      <button :disabled="searchLoading" @click="buttonSearch" class="form-control button-loading">
+        <span>Search</span>
+      </button>
+    </div>
   </div>
 </template>
 
